7c644967e10aae1e11f542a71d920de7c0c799f4
GIbson <njine10@gmail.com>
2021-03-31 12:23:19 -0400

    fetch crackle data and add it to a file

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import urllib.request
 import json
-apiKey="KiPGkbzEgEKUwhVXB3WARAco0prVUiCdWSuPiTUb"
+apiKey="SI4GiZabAskTGu45hy9LUzyQThhVeJzMor9iY3rD"
+apiKey2='VPj1VSW4gltUJXs40u0o7kdozDToKqYWco0a7zNc'
+apiKey3='YsD9TJSwufHmkcTOKqZ7SAPhdLirB8kRldcJ9pNB'
 
 
 def fetchSourceData():
@@ -28,17 +30,28 @@ def fetchGenreData():
       data = json.loads(url.read().decode())
       print(data) 
 
-def fetchTitleData():
-    with urllib.request.urlopen(f"https://api.watchmode.com/v1/title/345534/details/?apiKey={apiKey}") as url:
+def fetchTitleData(id,count):
+   current_api_key=apiKey
+   if  count % 2 == 0:
+     current_api_key=apiKey
+   elif count % 3:
+     current_api_key=apiKey2
+   else:
+     current_api_key=apiKey3
+
+   with urllib.request.urlopen(f"https://api.watchmode.com/v1/title/{id}/details/?apiKey={current_api_key}") as url:
       data = json.loads(url.read().decode())
       print(data)
+      if data:
+         return data
+      return
 
 def fetchlistTitleData():
     movies_array=[]
-    total_pages=57
+    total_pages=3
     page=1
-    while page<total_pages:
-        with urllib.request.urlopen(f"https://api.watchmode.com/v1/list-titles/?apiKey={apiKey}&source_ids=203,387,372,159,26&page={page}") as url:
+    while page<=total_pages:
+        with urllib.request.urlopen(f"https://api.watchmode.com/v1/list-titles/?apiKey={apiKey}&source_ids=77&page={page}") as url:
           data = json.loads(url.read().decode())
           print(page)
           movies_array+=data["titles"]
@@ -47,9 +60,26 @@ def fetchlistTitleData():
           page+=1
     return movies_array
 
+
 def writeToFile():
     data=fetchlistTitleData()
-    with open('data.txt', 'w') as outfile:
+    with open('crackle.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
+def fetchTitleDetails():
+    with open('crackle.txt') as json_file:
+     data = json.load(json_file)
+     titles=[]
+     count=0
+     for i in data:
+       print(count)
+       title=fetchTitleData(i['id'],count)
+       titles.append(title)
+       count+=1
+     return titles
+def saveTitleData():
+    data=fetchTitleDetails()
+    with open('crackletitle.txt', 'w') as outfile:
         json.dump(data, outfile)
 
 
@@ -58,8 +88,4 @@ def writeToFile():
 
 
 
-
-
-
-
-writeToFile()
+saveTitleData()
