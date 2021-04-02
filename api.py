2920cec2c157e505d9bb609b30d4107975f81684
Charlotte Ramiro <cramirodehuelbes@olin.edu>
2021-04-02 14:14:55 +0200

    Genre decoder function & merged files
    Created a function that decodes genres (input is the name of the genres
    and return is the encoded number) as well as a file-combiner function
    in order to get all our data in one file. I also refactored old function
    names into snake case.

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ testApi7="oWBeZ4Up3ESsGPZRn9wmlISCndVW1G2ZV7nbXakx"
 
 
 
-def fetchSourceData():
+def fetch_source_data():
 
   #params to take in 
     #types:this is the type of accounts offered i.e sub,free,purchase
@@ -23,22 +23,22 @@ def fetchSourceData():
       data = json.loads(url.read().decode())
       print(data)
 
-def fetchRegionData():
+def fetch_region_data():
     with urllib.request.urlopen(f"https://api.watchmode.com/v1/regions/?apiKey={apiKey}") as url:
       data = json.loads(url.read().decode())
       print(data)
 
-def fetchNetworkData():
+def fetch_network_data():
     with urllib.request.urlopen(f"https://api.watchmode.com/v1/networks/?apiKey={apiKey}") as url:
       data = json.loads(url.read().decode())
       print(data)   
 
-def fetchGenreData():
+def fetch_genre_data():
     with urllib.request.urlopen(f"https://api.watchmode.com/v1/genres/?apiKey={apiKey}") as url:
       data = json.loads(url.read().decode())
       print(data) 
 
-def fetchTitleData(id,count):
+def fetch_title_data(id,count):
   #  current_api_key=apiKey
   #  if  count % 2 == 0:
   #    current_api_key=apiKey
@@ -54,7 +54,7 @@ def fetchTitleData(id,count):
          return data
       return
 
-def fetchlistTitleData():
+def fetch_list_title_data():
     movies_array=[]
     total_pages=34
     page=1
@@ -69,12 +69,12 @@ def fetchlistTitleData():
     return movies_array
 
 
-def writeToFile():
-    data=fetchlistTitleData()
+def write_to_file():
+    data=fetch_list_title_data()
     with open('amazon.txt', 'w') as outfile:
         json.dump(data, outfile)
 
-def fetchTitleDetails(srcfile,source):
+def fetch_title_details(srcfile,source):
     with open(srcfile) as json_file:
      data = json.load(json_file)
      titles=[]
@@ -82,19 +82,20 @@ def fetchTitleDetails(srcfile,source):
      for i in data:
        if count <=100:
         print(count)
-        title=fetchTitleData(i['id'],count)
+        title=fetch_title_data(i['id'],count)
         title['source']= source
         titles.append(title)
         count+=1
        else:
          break 
      return titles
-def saveTitleData():
-    data=fetchTitleDetails("amazon.txt","Amazon Prime")
+
+def save_title_data():
+    data=fetch_title_details("amazon.txt","Amazon Prime")
     with open('amazontitles.txt', 'w') as outfile:
         json.dump(data, outfile)
 
-def analyzeTitleData():
+def analyze_title_data():
   with open('crackletitle.txt') as json_file:
      data = json.load(json_file)
      for i in data:
@@ -103,8 +104,69 @@ def analyzeTitleData():
        
 
 
+#Plotting functions
+
+#Merges all 5 individual files
+def merge_files(file1, file2, file3, file4, file5, combined_file):
+#I feel like theres a nicer way to have an unspecified number of args
+#but this will do for now
+  """
+  Merges the data from 5 files into 1. This is useful for analyzing all of the
+  data in a further function.
+
+  Args:
+  file1, file2, file3, file4, file5: all are strings representing the names of
+  files (including the extension) that are to be combined.
+  
+  combined_file: a string representing the name of the file to be created
+
+  Returns:
+  Nothing, but creates a file named
+  """
+  filenames = [file1, file2, file3, file4, file5]
+  with open(combined_file, 'w') as outfile:
+    
+    # Iterate through list
+    for names in filenames:
+    
+        # Open each file in read mode
+        with open(names) as infile:
+          outfile.write(infile.read())
+
+        outfile.write("\n")
+  pass
+
+def get_genre(genre, list=False):
+  """
+  Decodes genres and returns all movies of the inputted genre
+
+  Args: 
+  genre: a string containing the name of the genre
+
+  list: an optional argument in case the user wants to see the list of genres
+  that are available. It is a boolean. If list = True, then the list of genres
+  will be returned; otherwise, it will not be returned.
+
+  Returns:
+  A list of dictionaries of all of the movies across 5 streaming services that fit
+  within a genre
+  """
+  #return list of valid genres
+  #decoding the genre
+  with open("genre.txt") as json_file:
+    genre_data = json.load(json_file)
+    specific_genre = [item for item in genre_data if item["name"] == genre]
+    genre_number = specific_genre[0]["id"]
+
+  #get all movies with genre id "genre_number" 
+  with open("movie_details_list.txt") as json_file:
+    #CREATE MOVIE
+    movies_list = json.load(json_file)
+
+#getting list of movies
+pass
 
 
+#saveTitleData()
 
-
-saveTitleData()
+merge_files("disneyplustitles.txt", "amazontitles.txt", "netflixtitles.txt", "hbomaxtitles.txt", "hulutitles.txt")
