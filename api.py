042ec3b54ecd47b4987a870675784894e06243ae
Charlotte Ramiro <cramirodehuelbes@olin.edu>
2021-04-08 13:50:46 +0200

    Refactored data functions to take in arguments and decoded the source ids

--- api.py
+++ api.py
@@ -16,14 +16,37 @@ testApi6="rrFdXashTiwZVMCASk6aAgmMFjnb7JyXmcd9yglu"
 testApi7="oWBeZ4Up3ESsGPZRn9wmlISCndVW1G2ZV7nbXakx"
 
 
+def decode_source(source):
+  """
+  Gives the source id for a streaming service of a certain name. This helps
+  with API calls as the source id is needed to fetch data straight from the API
+
+  Args:
+  source: a string representing the name of the streaming service (i.e.
+  Netflix, HBO MAX)
+
+  Returns:
+  An integer representing the id of the source in the API.
+  """
+
+  with open("sources.txt") as json_file:
+    source_data = json.load(json_file)
+    specific_source = [item for item in source_data if item["name"] == source]
+    source_id = specific_source[0]["id"]
+  return source_id
+
+
 
 
 def fetch_title_data(id,count):
     """
-    This function takes in an id of a movie and sends a request to the watchmode api to get the details of the movie.
-    The API response is a dictionary with all the details of the said movie
+    This function takes in an id of a movie and sends a request to the
+    watchmode api to get the details of the movie. The API response is a
+    dictionary with all the details of said movie.
+
     Args:
-    id:this is the id of the movie
+    id: a string representing the id of the movie
+
     Returns:
     The function returns a dictionary with all the relevant details of a movie
     
@@ -36,7 +59,7 @@ def fetch_title_data(id,count):
 
     with urllib.request.urlopen(f"https://api.watchmode.com/v1/title/{id}/details/?apiKey={current_api_key}") as url:
         data = json.loads(url.read().decode())
-        print(data)
+        #print(data)
         if data:
           return data
         return
@@ -44,11 +67,14 @@ def fetch_title_data(id,count):
 def fetch_list_title_data(source_id):
     """
     
-    This function takes in a source id as a parameter and  sends a request to the watchmode api to get a list of all the movies from a certain source(Netflix,HBO).
-    Since the watchmode API returns data in pages, the function iterates through all the pages untill it has gotten all the data from that particular source.
+    This function takes in a source id as a parameter and  sends a request to
+    the watchmode api to get a list of all the movies from a certain source
+    (Netflix,HBO MAX). Since the watchmode API returns data in pages, the
+    function iterates through all the pages untill it has gotten all the data
+    from that particular source.
 
     Args:
-    source_id:this is the id of a particular source.For example, Netflix source id is 243
+    source_id: an integer representing the id of the streaming service
 
     Returns:
     The function returns a list of all the movies that exist in that particular source
@@ -60,35 +86,45 @@ def fetch_list_title_data(source_id):
     while page<=total_pages:
         with urllib.request.urlopen(f"https://api.watchmode.com/v1/list-titles/?apiKey={apiKey3}&source_ids={source_id}&page={page}") as url:
           data = json.loads(url.read().decode())
-          print(page)
+          #print(page)
           movies_array+=data["titles"]
           page+=1
     return movies_array
 
 
-def fetch_and_write_title_ids_to_file():
-    """
+def fetch_and_write_title_ids_to_file(source, filename):
+  """
 
-    This function writes all the movies from a specific source into one file.Once the `fetch_list_title_data` gets information from the watchmode API,
-    this function then writes that data into a file
+  This function writes all the movies from a specific source into one file.
+  Once the `fetch_list_title_data` gets information from the watchmode API,
+  this function then writes that data into a file.
 
-    """
-    data=fetch_list_title_data("243")
-    with open('amazon.txt', 'w') as outfile:
-        json.dump(data, outfile)
+  Args:
+  source: a string representing the name of the source.
+  filename: a string representing the file into which the data will be written
+
+  Returns:
+  Nothing, but writes the API data into the file.
+  """
+
+  source_id = decode_source(source)
+  data=fetch_list_title_data(source_id) #decode id
+  with open(filename, 'w') as outfile:
+    json.dump(data, outfile)
 
 def fetch_title_details(srcfile,source):
     """
 
-    This function utilises `fetch_title_data` to fetch data from the watchmode api, it then iterates through the list of dictionaries and 
-    appends the source attribute to each dictionary.
+    This function utilises `fetch_title_data` to fetch data from the watchmode
+    api, it then iterates through the list of dictionaries and appends the
+    source attribute to each dictionary.
 
     Args:
-      srcfile:This is the source fille to get data on all the movie titles
-      source:This is the name of the source i.e Netflix
+      srcfile:  This is the source file to get data on all the movie titles
+      source: This is the name of the streaming service i.e Netflix
 
     Returns:
-    The function returns all the titles of movies with the source they are from appended
+    The function returns a list of all of the movies with the service appended.
 
     """ 
     with open(srcfile) as json_file:
@@ -97,7 +133,7 @@ def fetch_title_details(srcfile,source):
      count=0
      for i in data:
        if count <=200:
-        print(count) 
+        #print(count) 
         title= fetch_title_data(i['id'],count)
         title['source']= source
         titles.append(title)
@@ -106,15 +142,24 @@ def fetch_title_details(srcfile,source):
          break 
      return titles
 
-def save_title_details_data():
+def save_title_details_data(source_file, service, new_file):
     """
 
-    This function writes all the movies titles from a specific source into one file.Once the `fetch_title_details` gets information from the watchmode API,
-    this function then writes that data into a file
+    This function writes all the movies titles from a specific source into one
+    file. Once the `fetch_title_details` gets information from the watchmode
+    API, this function then writes that data into a file.
+
+    Args:
+      source_file: a string with the file name, including the extension
+      (i.e. "netflix.txt")
+      service: a string representing the name of the streaming service
+      (i.e. "Netflix")
+      new_file: a string representing the name of the new file in which the
+      API data will be locally stored.
     
     """
-    data=fetch_title_details("netflix.txt","Netflix")
-    with open('netflixtitles1.txt', 'w') as outfile:
+    data=fetch_title_details(source_file, service)
+    with open(new_file, 'w') as outfile:
         json.dump(data, outfile)
 
 
@@ -414,49 +459,50 @@ def bubble_plot_genre(genre, student=False):
 
   #bubble plot is just scatter with variable bubble sizes
 
-def circular_barplot_prices(student=False):
-  """
-  Plots a circular barplot to show the prices of the streaming services.
+# def circular_barplot_prices(student=False):
+#   """
+#   Plots a circular barplot to show the prices of the streaming services.
 
-  Args:
-  No required arguments
-  student: a boolean value for whether you want to see the regular prices or
-  the special student prices. These prices are the cost per month.
+#   Args:
+#   No required arguments
+#   student: a boolean value for whether you want to see the regular prices or
+#   the special student prices. These prices are the cost per month.
 
-  Returns:
-  Nothing, but generates a circular barplot.
-  """
-  if student == True:
-    prices = [8.99, 2, 8.99, 14.99, 7.99]
-  else:
-    prices = [8.99, 6, 6.49, 14.99, 7.99]
+#   Returns:
+#   Nothing, but generates a circular barplot.
+#   """
+#   if student == True:
+#     prices = [8.99, 2, 8.99, 14.99, 7.99]
+#   else:
+#     prices = [8.99, 6, 6.49, 14.99, 7.99]
 
-  labels = ["Netflix", "Hulu", "Amazon Prime", "HBO MAX", "Disney Plus"]
-  max_price = max(prices)
-  print(max_price)
+#   labels = ["Netflix", "Hulu", "Amazon Prime", "HBO MAX", "Disney Plus"]
+#   max_price = max(prices)
+#   print(max_price)
 
-  #plotting time
-  ax = plt.subplot(111, polar=True)
-  plt.axis("off")
+#   #plotting time
+#   ax = plt.subplot(111, polar=True)
+#   plt.axis("off")
 
-  width = 2*np.pi / len(prices)
-  indexes=list(range(1, len(prices)+1))
-  angles = [element * width for element in indexes]
+#   width = 2*np.pi / len(prices)
+#   indexes=list(range(1, len(prices)+1))
+#   angles = [element * width for element in indexes]
 
-  #coordinate limits?
-  #draw bars
-  bars = ax.bars(
-    x=angles,
-    height=labels,
-    width=width,
-    bottom=0,
-    linewidth=2,
-    edgecolor="white")
+#   #coordinate limits?
+#   #draw bars
+#   bars = ax.bars(
+#     x=angles,
+#     height=labels,
+#     width=width,
+#     bottom=0,
+#     linewidth=2,
+#     edgecolor="white")
 
 
 # save_title_details_data()
 
 #STATEMENTS FOR FUNCTION TESTING
+#print(decode_source("Hulu"))
 # merge_files('amazontitles1.txt','disneyplustitles1.txt','hbomaxtitles1.txt','hulutitles1.txt','netflixtitles1.txt','movie_details_list1.txt')
 #print(number_of_movies("Horror", "Disney Plus"))
 # print(decode_genre("Horror"))
